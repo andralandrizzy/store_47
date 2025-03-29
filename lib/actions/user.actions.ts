beda5b1b86575b4c5ad103e05fe274dda1219aa2
andralandrizzy <andral.andrizzy@gmail.com>
2025-03-29 02:47:31 -0700

    Fix sign up issues

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,7 +9,6 @@ import {
 } from '../validators';
 import { auth, signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
-// import { hash } from '../encrypt';
 import { hashSync } from 'bcrypt-edge';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
